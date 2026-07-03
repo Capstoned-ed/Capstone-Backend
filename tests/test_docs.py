639cd6fef0ce09dd38e80823f0430c2b6e4821e6
nacht1234 <ralphperoch1789@gmail.com>
2026-07-03 15:29:40 +0800

    fix(ci): add trailing newline to test_docs

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,10 +1,10 @@
 from django.urls import reverse
 
+
 def test_api_docs_swagger_ui_loads(client):
     url = reverse('swagger-ui')
     response = client.get(url)
     assert response.status_code == 200
-
 
 
 def test_api_schema_loads(client):
